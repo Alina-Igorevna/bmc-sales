a122905c84bd7e41ade87b0dd8cd254ba579863a
indy1965 <dr.indy1965@yandex.ru>
2020-03-31 11:18:03 +0200

    day 2

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <?php 
+    $title = "BMC SALES";
+    include 'pages/head.php'; 
+  ?>
+</head>
+<body>
+  <?php include 'pages/header.php'; ?>
+  <main>
+
+  </main>
+</body>
+</html>
---
+++ pages/header.php
@@ -0,0 +1,17 @@
+<hader class="header">
+  <div class="container">
+    <div class="menu">
+      <div class="logo"><img src="img/logo.svg" alt="logo"></div>
+      <div class="nav menu__nav">
+        <a href="#" class="nav__item">Coц.сети <i class="fas fa-comment-dots"></i>
+        <a href="#" class="nav__item">Торгов.сети <i class="fas fa-cart-arrow-down"></i></a>
+        <a href="#" class="nav__item">Туристич.сети <i class="fas fa-globe"></i></a>
+        <a href="#" class="nav__item">Ассоциации <i class="fas fa-eye"></i></a>
+        <a href="#" class="nav__item">Сервисы <i class="fas fa-wrench"></i></a>
+        <i class="fab fa-android"></i>
+        <a href="#" class="nav__item">Вход на сайт <i class="fas fa-lock"></i></a>
+      </div>
+    </div>
+
+  </div>
+</hader>
